a906dcb1617becab7060c81bcf6eb85926d3c75a
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2023-03-13 11:51:02 +0100

    done task on functions part2a number 3 c

--- src/u02/task/function2a/Function.scala
+++ src/u02/task/function2a/Function.scala
@@ -14,18 +14,25 @@ object Function extends App:
     val negVal: (String => Boolean) => (String => Boolean) = predicate => !predicate(_)
     def negMethod(predicate: String => Boolean): String => Boolean = !predicate(_)
 
+    def neg[A](predicate: A => Boolean): A => Boolean = !predicate(_)
+
 
   import Function.*
 
   val empty: String => Boolean = _ == "" // predicate on strings
   val notEmptyVal = negVal(empty) // which type of notEmpty?
   val notEmptyMethod = negMethod(empty) // which type of notEmpty?
+  val notEmpty = neg(empty) // which type of notEmpty?
+
   println(notEmptyVal("foo")) // true
   println(notEmptyVal("")) // false
   println(notEmptyVal("foo") && !notEmptyVal("")) // true.. a comprehensive test
   println(notEmptyMethod("foo")) // true
   println(notEmptyMethod("")) // false
   println(notEmptyMethod("foo") && !notEmptyMethod("")) // true.. a comprehensive test
+  println(notEmpty("foo")) // true
+  println(notEmpty("")) // false
+  println(notEmpty("foo") && !notEmpty("")) // true.. a comprehensive test
 
   println(positiveVal(2))
   println(positiveMethod(-2))
