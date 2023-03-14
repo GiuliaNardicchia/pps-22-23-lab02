15992baa7d7804dc4f3569f822d3476fcde6fe17
Giulia Nardicchia <giulia.nardicchia@studio.unibo.it>
2023-03-14 10:32:06 +0100

    refactor code

--- src/u02/Optionals.scala
+++ src/u02/Optionals.scala
@@ -21,16 +21,15 @@ object Optionals extends App:
       case _ => None()
 
     def filter[A](opt: Option[A])(f: A => Boolean): Option[A] = opt match
-      case Some(a) if !isEmpty(Some(a)) && f(a) => Some(a)
+      case Some(a) if f(a) => Some(a)
       case _ => None()
 
-    def map[A](opt: Option[A])(f: A => Boolean): Option[Boolean] = opt match
-      case Some(a) if !isEmpty(Some(a)) && f(a) => Some(true)
-      case Some(a) if !isEmpty(Some(a)) && !f(a) => Some(false)
+    def map[A, B](opt: Option[A])(f: A => B): Option[B] = opt match
+      case Some(a) => Some(f(a))
       case _ => None()
 
     def fold[A](opt: Option[A])(default: A)(f: A => A): A = opt match
-      case Some(a) if !isEmpty(Some(a)) => f(a)
+      case Some(a) => f(a)
       case _ => default
 
   import Option.*
@@ -48,17 +47,13 @@ object Optionals extends App:
   println(flatMap(s1)(i => flatMap(s3)(j => Some(i + j)))) // None
   */
 
-  /*
   println(filter(Some(5))(_ > 2)) // Some(5)
   println(filter(Some(5))(_ > 8)) // None
   println(filter(None[Int]())(_ > 2)) // None
-  */
 
-  /*
   println(map(Some(5))(_ > 2)) // Some(true)
   println(map(Some(5))(_ > 8)) // Some(false)
   println(map(None[Int]())(_ > 2)) // None
-  */
 
   println(fold(Some(5))(1)(_ + 1)) // 6
   println(fold(None[Int]())(1)(_ + 1)) // 1
